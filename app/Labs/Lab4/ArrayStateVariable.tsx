bfc7a318bf1a94e530b227171b3693ba90e5e3f0
Azhar Abdulla <azhar.abdulla005@gmail.com>
2025-11-03 20:19:37 -0500

    fixes

--- app/Labs/Lab4/ArrayStateVariable.tsx
+++ app/Labs/Lab4/ArrayStateVariable.tsx
@@ -1,5 +1,8 @@
+"use client";
+
 import { useState } from "react";
 import { Button, ListGroup, ListGroupItem } from "react-bootstrap";
+
 import { useSelector } from "react-redux";
 import { RootState } from "./store";
 
